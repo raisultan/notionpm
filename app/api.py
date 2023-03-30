461f8b7b88ffd46236d124676b6115a5569a4c0e
raisultan <ki.xbozz@gmail.com>
2023-03-31 00:34:21 +0600

    aiogram is being used

--- app/api.py
+++ app/api.py
@@ -5,12 +5,17 @@ from dataclasses import dataclass
 
 from dotenv import load_dotenv
 from notion_client import Client as NotionCLI
+from aiogram import Bot, Dispatcher, executor, types
 
 load_dotenv()
 
 token = os.environ.get('NOTION_KEY')
 db_id = os.environ.get('NOTION_DB_ID')
 notion = NotionCLI(auth=token)
+bot_token = os.environ.get('BOT_TOKEN')
+
+bot = Bot(token=bot_token)
+dp = Dispatcher(bot)
 
 
 class Database(NamedTuple):
@@ -50,8 +55,27 @@ def list_pages_from(notion: NotionCLI, db_id: str) -> list[Page]:
         pages.append(
             Page(
                 id=block['id'],
-                title=block['properties']['Name']['title'][0]['plain_text'],
+                title=block['Name']['title'][0]['plain_text'],
                 properties=block['properties'],
             )
         )
     return pages
+
+
+@dp.message_handler(commands=['start'])
+async def tg_start(message: types.Message):
+    await message.answer("I'm here for you 🥷")
+
+
+@dp.message_handler(commands=['list'])
+async def tg_list_databases(message: types.Message):
+    databases = list_databases(notion)
+
+    answer = ''
+    for database in databases:
+        answer += f'{database.title}\n'
+    await message.answer(answer)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ app/database_pages_example.py
@@ -0,0 +1,144 @@
+[
+    Page(
+        id="396037fe-d152-44cd-ba0b-ad6ca82471d8",
+        title="Card 2",
+        properties={
+            "Assign": {"id": "HsYh", "type": "people", "people": []},
+            "Status": {
+                "id": "XdYK",
+                "type": "status",
+                "status": {
+                    "id": "a82472cb-56dc-408f-9a86-1837de945e22",
+                    "name": "In progress",
+                    "color": "blue",
+                },
+            },
+            "Name": {
+                "id": "title",
+                "type": "title",
+                "title": [
+                    {
+                        "type": "text",
+                        "text": {"content": "Card 2", "link": None},
+                        "annotations": {
+                            "bold": False,
+                            "italic": False,
+                            "strikethrough": False,
+                            "underline": False,
+                            "code": False,
+                            "color": "default",
+                        },
+                        "plain_text": "Card 2",
+                        "href": None,
+                    }
+                ],
+            },
+        },
+    ),
+    Page(
+        id="48c1483a-31a8-4f58-bfc5-05903d75dcb2",
+        title="Card 3",
+        properties={
+            "Assign": {
+                "id": "HsYh",
+                "type": "people",
+                "people": [
+                    {
+                        "object": "user",
+                        "id": "bd83f89f-62c0-4193-b9ae-ed2ff84eaaec",
+                        "name": "Kayrat Medeshov",
+                        "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/2fa16287-9705-4ef0-82fa-050be2ec859c/_-1.jpg",
+                        "type": "person",
+                        "person": {"email": "oklimova-ki18@stud.sfu-kras.ru"},
+                    }
+                ],
+            },
+            "Status": {
+                "id": "XdYK",
+                "type": "status",
+                "status": {
+                    "id": "57fefebb-e836-4b0f-b964-80a3a8ddba08",
+                    "name": "Done",
+                    "color": "green",
+                },
+            },
+            "Name": {
+                "id": "title",
+                "type": "title",
+                "title": [
+                    {
+                        "type": "text",
+                        "text": {"content": "Card 3", "link": None},
+                        "annotations": {
+                            "bold": False,
+                            "italic": False,
+                            "strikethrough": False,
+                            "underline": False,
+                            "code": False,
+                            "color": "default",
+                        },
+                        "plain_text": "Card 3",
+                        "href": None,
+                    }
+                ],
+            },
+        },
+    ),
+    Page(
+        id="6db3b5ea-dd5d-4378-ae77-a5d94f11c47a",
+        title="Card 1",
+        properties={
+            "Assign": {
+                "id": "HsYh",
+                "type": "people",
+                "people": [
+                    {
+                        "object": "user",
+                        "id": "346a04af-8943-4e0d-940d-3fd186020991",
+                        "name": "raisultan",
+                        "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/2c4ed0ff-1a60-413d-b32e-0f08288cf854/Screenshot_2021-06-18_at_13.41.45.png",
+                        "type": "person",
+                        "person": {"email": "raisultan@proton.me"},
+                    },
+                    {
+                        "object": "user",
+                        "id": "bd83f89f-62c0-4193-b9ae-ed2ff84eaaec",
+                        "name": "Kayrat Medeshov",
+                        "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/2fa16287-9705-4ef0-82fa-050be2ec859c/_-1.jpg",
+                        "type": "person",
+                        "person": {"email": "oklimova-ki18@stud.sfu-kras.ru"},
+                    },
+                ],
+            },
+            "Status": {
+                "id": "XdYK",
+                "type": "status",
+                "status": {
+                    "id": "4c4d922d-319f-415a-9b38-c3882b2adf27",
+                    "name": "Not started",
+                    "color": "default",
+                },
+            },
+            "Name": {
+                "id": "title",
+                "type": "title",
+                "title": [
+                    {
+                        "type": "text",
+                        "text": {"content": "Card 1", "link": None},
+                        "annotations": {
+                            "bold": False,
+                            "italic": False,
+                            "strikethrough": False,
+                            "underline": False,
+                            "code": False,
+                            "color": "default",
+                        },
+                        "plain_text": "Card 1",
+                        "href": None,
+                    }
+                ],
+            },
+        },
+    ),
+]
