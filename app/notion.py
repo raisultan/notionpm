9a598993e38e1b61617094fa13173be2c538a229
raisultan <ki.xbozz@gmail.com>
2023-04-03 21:18:55 +0600

    api.py -> notion.py

---
+++ app/notion.py
@@ -0,0 +1,57 @@
+import os
+from dataclasses import dataclass
+from typing import NamedTuple
+
+from dotenv import load_dotenv
+from notion_client import Client as NotionCLI
+
+load_dotenv()
+
+token = os.environ.get('NOTION_KEY')
+db_id = os.environ.get('NOTION_DB_ID')
+notion = NotionCLI(auth=token)
+bot_token = os.environ.get('BOT_TOKEN')
+
+
+class Database(NamedTuple):
+    id: str
+    title: str
+
+
+@dataclass
+class Page:
+    id: str
+    title: str
+    properties: dict
+
+
+def list_databases(notion: NotionCLI) -> list[Database]:
+    blocks = notion.search()
+    databases = []
+
+    for block in blocks['results']:
+        # see search_example.py
+        if block['object'] == 'database':
+            databases.append(
+                Database(
+                    id=block['id'],
+                    title=block['title'][0]['plain_text'],
+                ),
+            )
+
+    return databases
+
+
+def list_pages_from(notion: NotionCLI, db_id: str) -> list[Page]:
+    blocks = notion.databases.query(db_id)
+    pages = []
+
+    for block in blocks['results']:
+        pages.append(
+            Page(
+                id=block['id'],
+                title=block['Name']['title'][0]['plain_text'],
+                properties=block['properties'],
+            )
+        )
+    return pages
