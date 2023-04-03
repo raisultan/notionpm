54050d081bb2e79fdae78c66d610614f7e21e54a
raisultan <ki.xbozz@gmail.com>
2023-04-03 21:17:18 +0600

    a bit of refactoring done on server.py

--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+import asyncio
 import base64
 import os
 
@@ -15,98 +16,94 @@ bot = Bot(token=os.environ["BOT_TOKEN"])
 dp = Dispatcher(bot)
 notion = Client(auth=os.environ["NOTION_KEY"])
 
-NOTION_CLIENT_ID = os.environ["OAUTH_CLIENT_ID"]
-NOTION_CLIENT_SECRET = os.environ["OAUTH_CLIENT_SECRET"]
-NOTION_REDIRECT_URI = os.environ["CALLBACK_URL"]
+NOTION_CLIENT_ID = os.environ["NOTION_CLIENT_ID"]
+NOTION_CLIENT_SECRET = os.environ["NOTION_CLIENT_SECRET"]
+NOTION_REDIRECT_URI = os.environ["NOTION_REDIRECT_URI"]
+BOT_URL = os.environ["BOT_URL"]
 
 
-async def handle(request: Request):
-    if request.method != "GET":
-        return web.Response(status=405)
+async def make_oauth_request(code: str):
+    auth_creds = f"{NOTION_CLIENT_ID}:{NOTION_CLIENT_SECRET}"
+    encoded_auth_creds = base64.b64encode(auth_creds.encode("ascii")).decode("ascii")
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {encoded_auth_creds}",
+    }
+    json = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": NOTION_REDIRECT_URI,
+    }
+
+    http_client = aiohttp.ClientSession()
+    response = await http_client.post(
+        "https://api.notion.com/v1/oauth/token",
+        json=json,
+        headers=headers,
+    )
+    return await response.json()
+
+
+async def handle_oauth(request: Request):
     code = request.query.get("code")
-    chat_id = request.query.get("state").split("-")[1]
-    url = "https://t.me/notionnBot"
-    if code and chat_id:
-        async with aiohttp.ClientSession() as session:
-            # Exchange the authorization code for an access token
-            json = {
-                "grant_type": "authorization_code",
-                "code": code,
-                "redirect_uri": NOTION_REDIRECT_URI,
-            }
-            credentials = f"{NOTION_CLIENT_ID}:{NOTION_CLIENT_SECRET}"
-            credentials_bytes = credentials.encode("ascii")
-            # Base64-encode the bytes object
-            encoded_credentials_bytes = base64.b64encode(credentials_bytes)
-            # Convert the encoded bytes object to a string
-            encoded_credentials_str = encoded_credentials_bytes.decode("ascii")
-            print(json)
-            response = await session.post(
-                "https://api.notion.com/v1/oauth/token",
-                json=json,
-                headers={
-                    "Content-Type": "application/json",
-                    "Authorization": f"Basic {encoded_credentials_str}",
-                },
-            )
-            data = await response.json()
-            print("-------------------------")
-            print(data)
-            print("-------------------------")
-            access_token = data.get("access_token")
-            print("ACCESS TOKEN:")
-            print(access_token)
-            notion = Client(auth=access_token)
-            blocks = notion.search()
-            print(blocks)
+    state = request.query.get("state")
+    if state and "-" in state:
+        chat_id = state.split("-")[1]
+    else:
+        chat_id = None
 
-            await bot.send_message(chat_id, "Login successful!")
+    if not (code and chat_id):
+        return web.Response(status=400)
 
-        return web.HTTPFound(url)
-    return web.Response(status=400)
+    try:
+        response = await make_oauth_request(code)
+
+        access_token = response.get("access_token")
+        notion = Client(auth=access_token)
+        blocks = notion.search()
+        print(blocks)
+
+        await bot.send_message(chat_id, "Login successful!")
+        return web.HTTPFound(BOT_URL)
+    except Exception as e:
+        print(e)
+        return web.Response(status=400)
 
 
 async def send_welcome(message: types.Message):
-    await bot.send_message(
-        message.chat.id,
-        "Hi there! I'm a bot that can help you with Notion OAuth login. Just type /login to get started.",
+    reply = (
+        "Hi there! I'm a bot that can help you with Notion "
+        "OAuth login. Just type /login to get started."
     )
+    await bot.send_message(message.chat.id, reply)
 
 
 async def send_login_url(message: types.Message):
-    chat_id = message.chat.id
-    login_url = f"https://api.notion.com/v1/oauth/authorize?client_id={NOTION_CLIENT_ID}&redirect_uri={NOTION_REDIRECT_URI}&response_type=code&state=instance-{chat_id}"
-    await bot.send_message(
-        message.chat.id,
-        f"Click the link to login to Notion:\n\n{login_url}",
-        parse_mode=ParseMode.HTML,
+    login_url = (
+        "https://api.notion.com/v1/oauth/authorize"
+        f"?client_id={NOTION_CLIENT_ID}"
+        f"&redirect_uri={NOTION_REDIRECT_URI}"
+        f"&response_type=code"
+        f"&state=instance-{message.chat.id}"
     )
-
-
-async def handle_notion_callback(message: types.Message):
-    # Extract authorization code from URL
-    auth_code = message.text.split("=")[1]
-
-    notion = Client(auth=auth_code)
-    await message.answer("Login successful!")
+    reply = f"Click the link to login to Notion:\n\n{login_url}"
+    await bot.send_message(message.chat.id, reply, parse_mode=ParseMode.HTML)
 
 
 dp.register_message_handler(send_welcome, commands=["start"])
 dp.register_message_handler(send_login_url, commands=["login"])
-dp.register_message_handler(handle_notion_callback, regexp=r"code=[a-zA-Z0-9]+")
+
+
+async def main():
+    app = web.Application()
+    app.add_routes([web.get("/oauth/callback", handle_oauth)])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    tcp_server = web.TCPSite(runner, "localhost", 8080)
+
+    await tcp_server.start()
+    await dp.start_polling()
+
 
 if __name__ == "__main__":
-
-    async def main():
-        app = web.Application()
-        app.add_routes([web.get("/oauth/callback", handle)])
-        runner = web.AppRunner(app)
-        await runner.setup()
-        site = web.TCPSite(runner, "localhost", 8080)
-        await site.start()
-        await dp.start_polling()
-
-    # Run the main coroutine until it completes
-    import asyncio
-
     asyncio.run(main())
