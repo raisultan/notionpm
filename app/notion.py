93d7706fc3eadf05196c12e884154df601e06a0b
raisultan <ki.xbozz@gmail.com>
2023-04-07 18:33:11 +0600

    tracking supports some prop types

--- app/notion.py
+++ app/notion.py
@@ -63,9 +63,24 @@ notion = NotionCLI(NOTION_TOKEN)
 NOTION_DB = os.environ["NOTION_DB_ID"]
 
 from copy import deepcopy
+from typing import Any
 
 
-def track_changes(old: list[dict], new: list[dict], props: list[str]):
+@dataclass
+class PropertyChange:
+    name: str
+    old_value: Any
+    new_value: Any
+
+
+@dataclass
+class PageChange:
+    name: str
+    url: str
+    field_changes: list[PropertyChange]
+
+
+def track_db_changes(old: list[dict], new: list[dict], props: list[str]) -> list[PageChange]:
     old, new = deepcopy(old), deepcopy(new)
 
     # get page id lists for old and new
@@ -98,12 +113,55 @@ def track_changes(old: list[dict], new: list[dict], props: list[str]):
     old_for_tracking_props = sorted(old_for_tracking_props, key=lambda page: page['id'])
     new_for_tracking_props = sorted(new_for_tracking_props, key=lambda page: page['id'])
 
+    db_changes = []
     for old_page, new_page in zip(old_for_tracking_props, new_for_tracking_props):
         old_page_props = old_page['properties']
         new_page_props = new_page['properties']
+        page_property_changes = []
         for prop in props:
             if old_page_props[prop] != new_page_props[prop]:
-                print(
-                    f'Property "{prop}" of page "{new_page_props["Name"]["title"][0]["plain_text"]}" '
-                    f'changed from "{old_page_props[prop]}" to "{new_page_props[prop]}"'
+                old_value, new_value = track_change_on_property(old_page_props[prop], new_page_props[prop])
+                page_property_changes.append(PropertyChange(prop, old_value, new_value))
+        if page_property_changes:
+            db_changes.append(
+                PageChange(
+                    get_page_name(old_page),
+                    get_page_url(old_page),
+                    page_property_changes,
                 )
+            )
+    return db_changes
+
+
+def track_change_on_property(old: dict, new: dict) -> tuple:
+    if old['type'] == 'title':
+        old = old['title'][0]['plain_text']
+        new = new['title'][0]['plain_text']
+        return old, new
+    elif old['type'] == 'status':
+        old = old['status']['name']
+        new = new['status']['name']
+        return old, new
+    elif old['type'] == 'date':
+        old_start = old['date']['start']
+        new_start = new['date']['start']
+        old_end = old['date']['end']
+        new_end = new['date']['end']
+        return f'{old_start} -> {old_end}', f'{new_start} -> {new_end}'
+    elif old['type'] == 'people':
+        old = [person['name'] for person in old['people']]
+        new = [person['name'] for person in new['people']]
+        return old, new
+    elif old['type'] == 'url':
+        old = old['url']
+        new = new['url']
+        return old, new
+    else:
+        return 'unknown', 'unknown'
+
+
+def get_page_name(page: dict) -> str:
+    return page['properties']['Name']['title'][0]['plain_text']
+
+def get_page_url(page: dict) -> str:
+    return page['url']
