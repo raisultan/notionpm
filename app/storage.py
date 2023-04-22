197405947d57eab7b05362385f75c8fddc7e5ae1
raisultan <ki.xbozz@gmail.com>
2023-04-22 12:53:02 +0600

    storage refactoring

--- app/storage.py
+++ app/storage.py
@@ -7,11 +7,11 @@ redis = aioredis.from_url("redis://localhost")
 
 
 async def save_user_access_token(chat_id: str, access_token: str) -> None:
-    await redis.set(f'chat_{chat_id}', access_token)
+    await redis.set(f'access_token_{chat_id}', access_token)
 
 
 async def get_user_access_token(chat_id: str) -> Optional[str]:
-    token = await redis.get(f'chat_{chat_id}')
+    token = await redis.get(f'access_token_{chat_id}')
     if not token:
         return None
     return token.decode('utf-8')
@@ -25,11 +25,11 @@ async def get_all_chat_ids() -> list:
 
 
 async def set_user_db_id(chat_id: str, db_id: str) -> None:
-    await redis.set(f'db_{chat_id}', db_id)
+    await redis.set(f'db_id_{chat_id}', db_id)
 
 
 async def get_user_db_id(chat_id: str) -> Optional[str]:
-    db_id = await redis.get(f'db_{chat_id}')
+    db_id = await redis.get(f'db_id_{chat_id}')
     if not db_id:
         return None
     return db_id.decode('utf-8')
@@ -52,67 +52,32 @@ async def set_user_tracked_properties(chat_id: str, tracked_properties: list) ->
 
 async def get_user_tracked_properties(chat_id: str) -> Optional[list]:
     tracked_properties = await redis.get(f'tracked_properties_{chat_id}')
-    print(tracked_properties)
     if not tracked_properties:
         return None
     return json.loads(tracked_properties)
 
 
 async def set_tracked_properties_message_id(chat_id: str, message_id: int) -> None:
-    user_key = f"user:{chat_id}"
-    user_data = await redis.get(user_key)
-
-    if user_data:
-        user_data = json.loads(user_data)
-    else:
-        user_data = {}
-
-    user_data["tracked_properties_message_id"] = message_id
-    await redis.set(user_key, json.dumps(user_data))
+    await redis.set(f'tracked_properties_message_id_{chat_id}', message_id)
 
 
 async def get_tracked_properties_message_id(chat_id: str) -> Optional[int]:
-    user_key = f"user:{chat_id}"
-    user_data = await redis.get(user_key)
-
-    if user_data:
-        user_data = json.loads(user_data)
-        message_id = user_data.get("tracked_properties_message_id")
-        return message_id
-    else:
+    message_id = await redis.get(f'tracked_properties_message_id_{chat_id}')
+    if not message_id:
         return None
+    return int(message_id)
 
 
 async def delete_tracked_properties_message_id(chat_id: str) -> None:
-    user_key = f"user:{chat_id}"
-    user_data = await redis.get(user_key)
-
-    if user_data:
-        user_data = json.loads(user_data)
-        if "tracked_properties_message_id" in user_data:
-            user_data["tracked_properties_message_id"] = None
-            await redis.set(user_key, json.dumps(user_data))
+    await redis.delete(f'tracked_properties_message_id_{chat_id}')
 
 
 async def set_user_setup_status(chat_id: str, is_in_setup: bool) -> None:
-    user_key = f"user:{chat_id}"
-    user_data = await redis.get(user_key)
-
-    if user_data:
-        user_data = json.loads(user_data)
-    else:
-        user_data = {}
-
-    user_data["in_setup"] = is_in_setup
-    await redis.set(user_key, json.dumps(user_data))
+    await redis.set(f'in_setup_{chat_id}', is_in_setup)
 
 
 async def get_user_setup_status(chat_id: str) -> bool:
-    user_key = f"user:{chat_id}"
-    user_data = await redis.get(user_key)
-
-    if user_data:
-        user_data = json.loads(user_data)
-        return user_data.get("in_setup", False)
-    else:
+    is_in_setup = await redis.get(f'in_setup_{chat_id}')
+    if not is_in_setup:
         return False
+    return bool(is_in_setup)
