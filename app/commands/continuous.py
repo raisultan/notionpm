2811f487a4205a6f5b5781416c6f4a54e5237396
raisultan <ki.xbozz@gmail.com>
2023-04-30 03:41:52 +0600

    keyword argument wrong?

--- app/commands/continuous.py
+++ app/commands/continuous.py
@@ -1,5 +1,7 @@
 from typing import Any, Awaitable
 
+from aiogram.types import CallbackQuery
+
 
 class ContinuousCommand:
     def __init__(
@@ -13,13 +15,13 @@ class ContinuousCommand:
         self._finish = finish
         self._next = next
 
-    async def finish(self, *args, **kwargs) -> None:
-        is_finished = await self._finish(*args, **kwargs)
+    async def finish(self, query: CallbackQuery) -> None:
+        is_finished = await self._finish(query)
         if (
             is_finished
             and not await self._next.is_finished()
             and await self._next.is_applicable()
         ):
-            await self._next.execute(*args, **kwargs)
+            await self._next.execute(query.message)
         else:
             return None
