8cda7ec7a76f733dadfa19f9b5f3ec974e794c51
raisultan <ki.xbozz@gmail.com>
2023-04-30 03:16:18 +0600

    remove trash

--- app/commands/start.py
+++ app/commands/start.py
@@ -1,9 +1,11 @@
+from aiogram import Bot
 from aiogram.types import Message
-
-from app.commands.common import skip_or_continue_setup
 
 
 class StartCommand:
+    def __init__(self, bot: Bot):
+        self._bot = bot
+
     async def is_applicable(self) -> bool:
         return True
 
@@ -13,4 +15,7 @@ class StartCommand:
     async def execute(self, message: Message) -> None:
         if message.chat.type != "private":
             return
-        await skip_or_continue_setup(message)
+        await self._bot.send_message(
+            message.chat.id,
+            "Hi there! Good to see you here 😊\n",
+        )
