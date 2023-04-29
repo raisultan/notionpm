c043fe0ba30d20350b6a2686cec8c224e4ac3da1
raisultan <ki.xbozz@gmail.com>
2023-04-29 16:59:51 +0600

    try dependency injection by hand

--- app/commands/choose_database.py
+++ app/commands/choose_database.py
@@ -1,86 +1,94 @@
-from aiogram import types
+from typing import Any, Final
+
+from aiogram import Bot
 from aiogram.types import (
     CallbackQuery,
     InlineKeyboardButton,
     InlineKeyboardMarkup,
+    Message,
 )
 from aiogram.utils.callback_data import CallbackData
 from notion_client import Client as NotionCLI
 
-import app.storage as storage
-from app.notion import list_databases
-
 from app.commands.common import skip_or_continue_setup
-from app.initializer import bot
+
+ChooseDatabaseCallback: Final[CallbackData] = CallbackData("choose_db", "db_id", "db_title")
 
 
-choose_db_callback_data = CallbackData("choose_db", "db_id", "db_title")
-async def choose_database_handler(message: types.Message):
-    chat_id = message.chat.id
-    access_token = await storage.get_user_access_token(chat_id)
+class ChooseDatabaseCommand:
+    """Command to choose a database to track."""
 
-    # TODO: repeated code
-    if not access_token:
-        await bot.send_message(
+    def __init__(self, bot: Bot, storage: Any, notion: NotionCLI, notion_cli: Any):
+        self._bot = bot
+        self._storage = storage
+        self._notion = notion
+        self._notion_cli = notion_cli
+
+    async def execute(self, message: Message) -> None:
+        chat_id = message.chat.id
+        access_token = await self._storage.get_user_access_token(chat_id)
+
+        # TODO: repeated code
+        if not access_token:
+            await self._bot.send_message(
+                chat_id,
+                "You need to connect your Notion workspace first. Use the /login command to connect.",
+            )
+            return
+
+        user_notion = self._notion(auth=access_token)
+        databases = self._notion_cli.list_databases(user_notion)
+
+        # TODO: repeated code
+        if not databases:
+            await self._bot.send_message(message.chat.id, "No databases found in your Notion workspace.")
+            return
+
+        connect_message_id = await self._storage.get_connect_message_id(message.chat.id)
+        if connect_message_id:
+            await self._bot.delete_message(chat_id, connect_message_id)
+
+        if len(databases) == 1:
+            db = databases[0]
+            await self._storage.set_user_db_id(message.chat.id, db.id)
+            await self._bot.send_message(
+                message.chat.id,
+                f"Yeah, default database has been set to {db.title} 🎉",
+            )
+            await skip_or_continue_setup(message)
+            return
+
+        inline_keyboard = []
+
+        for db in databases:
+            button = InlineKeyboardButton(
+                db.title,
+                callback_data=ChooseDatabaseCallback.new(db_id=db.id, db_title=db.title),
+            )
+            inline_keyboard.append([button])
+
+        markup = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+        current_db_id = await self._storage.get_user_db_id(message.chat.id)
+        if current_db_id:
+            text = (
+                "You have already chosen a default database. "
+                "You can choose a new one from the list below:"
+            )
+            await self._bot.send_message(message.chat.id, text, reply_markup=markup)
+        else:
+            await self._bot.send_message(
+                message.chat.id,
+                "Choose the default database for team tasks:",
+                reply_markup=markup,
+            )
+
+    async def handle_callback(self, query: CallbackQuery) -> None:
+        db_id = query.get("db_id")
+        chat_id = query.message.chat.id
+
+        await self._storage.set_user_db_id(chat_id, db_id)
+        await self._bot.send_message(
             chat_id,
-            "You need to connect your Notion workspace first. Use the /login command to connect.",
+            f"Default database has been set to {query.get('db_title')} 🎉",
         )
-        return
-
-    user_notion = NotionCLI(auth=access_token)
-    databases = list_databases(user_notion)
-
-    # TODO: repeated code
-    if not databases:
-        await bot.send_message(message.chat.id, "No databases found in your Notion workspace.")
-        return
-
-    connect_message_id = await storage.get_connect_message_id(message.chat.id)
-    if connect_message_id:
-        await bot.delete_message(chat_id, connect_message_id)
-
-    if len(databases) == 1:
-        db = databases[0]
-        await storage.set_user_db_id(message.chat.id, db.id)
-        await bot.send_message(
-            message.chat.id,
-            f"Yeah, default database has been set to {db.title} 🎉",
-        )
-        await skip_or_continue_setup(message)
-        return
-
-    inline_keyboard = []
-
-    for db in databases:
-        button = InlineKeyboardButton(
-            db.title,
-            callback_data=choose_db_callback_data.new(db_id=db.id, db_title=db.title),
-        )
-        inline_keyboard.append([button])
-
-    markup = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
-    current_db_id = await storage.get_user_db_id(message.chat.id)
-    if current_db_id:
-        text = (
-            "You have already chosen a default database. "
-            "You can choose a new one from the list below:"
-        )
-        await bot.send_message(message.chat.id, text, reply_markup=markup)
-    else:
-        await bot.send_message(
-            message.chat.id,
-            "Choose the default database for team tasks:",
-            reply_markup=markup,
-        )
-
-
-async def choose_db_callback_handler(callback_query: CallbackQuery, callback_data: dict):
-    db_id = callback_data.get("db_id")
-    chat_id = callback_query.message.chat.id
-
-    await storage.set_user_db_id(chat_id, db_id)
-    await bot.send_message(
-        chat_id,
-        f"Default database has been set to {callback_data.get('db_title')} 🎉",
-    )
-    await skip_or_continue_setup(callback_query.message)
+        await skip_or_continue_setup(query.message)
--- app/dispatcher.py
+++ app/dispatcher.py
@@ -4,9 +4,21 @@ from app.initializer import bot
 
 from app.commands.start import send_welcome
 from app.commands.connect_notion import send_login_url
-from app.commands.choose_database import choose_database_handler, choose_db_callback_handler, choose_db_callback_data
+from app.commands.choose_database import ChooseDatabaseCallback, ChooseDatabaseCommand
 from app.commands.choose_properties import choose_properties_handler, choose_property_callback_handler, choose_property_callback_data, properties_done_handler
 from app.commands.set_notifications import set_notification_handler, set_notification_callback_handler, set_notification_callback_data, on_chat_member_updated
+
+from app.initializer import bot
+import app.storage as storage
+from notion_client import Client as NotionCLI
+import app.notion as notion_cli
+
+choose_database = ChooseDatabaseCommand(
+    bot=bot,
+    storage=storage,
+    notion=NotionCLI,
+    notion_cli=notion_cli,
+)
 
 
 def setup_dispatcher():
@@ -21,12 +33,12 @@ def setup_dispatcher():
         commands=["login"],
     )
     dp.register_message_handler(
-        choose_database_handler,
+        choose_database.execute,
         commands=["choose_database"],
     )
     dp.register_callback_query_handler(
-        choose_db_callback_handler,
-        choose_db_callback_data.filter()
+        choose_database.handle_callback,
+        ChooseDatabaseCallback.filter()
     )
     dp.register_message_handler(
         choose_properties_handler,
