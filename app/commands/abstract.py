09554db98c40ff21219db427c4f8b21d583b86c2
raisultan <ki.xbozz@gmail.com>
2023-05-01 20:23:54 +0600

    using query in checkers, composing message for next

--- app/commands/abstract.py
+++ app/commands/abstract.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Optional
 
 from aiogram import Bot
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, Chat, User
 
 
 class AbstractCommand:
@@ -13,18 +13,25 @@ class AbstractCommand:
     async def execute(self, message: Message) -> None:
         raise NotImplementedError
 
-    async def is_applicable(self, message: Message) -> bool:
+    async def is_applicable(self, query: CallbackQuery) -> bool:
         raise NotImplementedError
 
-    async def is_finished(self, message: Message) -> bool:
+    async def is_finished(self, query: CallbackQuery) -> bool:
         raise NotImplementedError
 
     async def execute_next_if_applicable(self, query: CallbackQuery) -> None:
-        print(f'\n\n{self} APPLICABLE: {await self._next.is_applicable(query.message)}, FINISHED: {not bool(await self._next.is_finished(query.message))}\n\n')
         if (
-            await self._next.is_applicable(query.message)
-            and not await self._next.is_finished(query.message)
+            await self._next.is_applicable(query)
+            and not await self._next.is_finished(query)
         ):
-            await self._next.execute(query.message)
+            chat = Chat(id=query.message.chat.id, type='private')
+            user = query.from_user
+            message_data = {
+                'chat': chat.to_python(),
+                'from': user.to_python(),
+                'message_id': 0,
+            }
+
+            await self._next.execute(Message(**message_data))
         else:
             return None
