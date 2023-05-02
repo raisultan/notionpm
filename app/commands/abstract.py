f32a093f06f8de5b3bc854768d12da2879e59fa9
raisultan <ki.xbozz@gmail.com>
2023-05-02 21:27:30 +0600

    remove excessive logic

--- app/commands/abstract.py
+++ app/commands/abstract.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Optional
 
 from aiogram import Bot
-from aiogram.types import CallbackQuery, Message, Chat, User
+from aiogram.types import Message
 
 
 class AbstractCommand:
@@ -19,18 +19,11 @@ class AbstractCommand:
     async def is_finished(self, message: Message) -> bool:
         raise NotImplementedError
 
-    async def execute_next_if_applicable(self, query: CallbackQuery) -> None:
+    async def execute_next_if_applicable(self, message: Message) -> None:
         if (
-            await self._next.is_applicable(query.message)
-            and not await self._next.is_finished(query.message)
+            await self._next.is_applicable(message)
+            and not await self._next.is_finished(message)
         ):
-            chat = Chat(id=query.message.chat.id, type='private')
-            user = User(id=query.from_user.id, is_bot=False, first_name='dummy', username='dummy')
-            message_data = {
-                'chat': chat.to_python(),
-                'from': user.to_python(),
-                'message_id': 0,
-            }
-            await self._next.execute(Message(**message_data))
+            await self._next.execute(message)
         else:
             return None
