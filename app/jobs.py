24820852546957ee70a63edd55fdb16d14372009
Raisultan <ki.xbozz@gmail.com>
2023-05-10 23:06:39 +0600

    decent setup (#7)
    * dispatcher setup better
    
    * promising setup way
    
    * bot is alive, some fixes made
    
    * dispatcher works once and doesn't work the next time, interesting bug, but jobs are transfered
    
    * examples are not needed anymore
    
    * some cleaning in the repo 🚧
    
    * setup fixed 🚀🦆

---
+++ app/jobs.py
@@ -0,0 +1,15 @@
+from aiohttp.web import Application
+from rocketry import Rocketry
+from rocketry.conds import every
+
+from app.notifications import track_changes_for_all
+
+
+def setup_jobs(app: Application, scheduler: Rocketry) -> None:
+    params = {'app': app}
+
+    scheduler.session.create_task(
+        func=track_changes_for_all,
+        start_cond=every('15 seconds'),
+        parameters=params,
+    )
